6ae446181b00bd2f12d5cb53a471b65af0d8f42c
joaosantosmedeiros <jopesame@gmail.com>
2024-07-17 09:26:48 -0300

    feat: make pokemon detail scrollable

--- lib/pages/pokemon_detail.dart
+++ lib/pages/pokemon_detail.dart
@@ -16,191 +16,195 @@ class PokemonDetail extends StatelessWidget {
         '#${'0' * (3 - pokemon.id.toString().length)}${pokemon.id}';
 
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: mainColor,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                title,
-                style:
-                    const TextStyle(color: Colors.white, fontFamily: 'Pokemon'),
-              ),
-              Text(
-                pokemonNumber,
-                style:
-                    const TextStyle(color: Colors.white, fontFamily: 'Pokemon'),
-              ),
-            ],
-          )),
-      body: Column(
-        children: [
-          // Imagem
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(30),
-                        bottomRight: Radius.circular(30)),
-                    color: mainColor,
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [Image.network(pokemon.imageUrl)],
-                  ),
-                ),
-              ),
-            ],
-          ),
-          // Tipos
-          Container(
-            margin: const EdgeInsets.only(top: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: pokemon.types
-                  .map(
-                    (type) => Container(
-                      margin: const EdgeInsets.only(right: 3.0, left: 3.0),
-                      padding: const EdgeInsets.only(
-                          top: 2.0, right: 10.0, bottom: 2.0, left: 10.0),
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(
-                          255,
-                          TypeToColor.convert(type)[0],
-                          TypeToColor.convert(type)[1],
-                          TypeToColor.convert(type)[2],
-                        ),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(15.0)),
-                      ),
-                      child: Text(
-                        type[0].toUpperCase() + type.substring(1, type.length),
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                  )
-                  .toList(),
-            ),
-          ),
-          // Peso, altura e habilidades
-          Container(
-            margin: const EdgeInsets.only(top: 10, bottom: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Weight
-                Container(
-                  margin: const EdgeInsets.only(left: 10, right: 10),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 70,
-                        child: Row(
-                          children: [
-                            const Icon(
-                              Icons.balance_rounded,
-                              color: Colors.grey,
-                            ),
-                            Text("${pokemon.weight}kg"),
-                          ],
-                        ),
-                      ),
-                      Text(
-                        "Weight",
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ),
-                // Height
-                Container(
-                  margin: const EdgeInsets.only(left: 10, right: 10),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 70,
-                        child: Row(
-                          children: [
-                            const Icon(
-                              Icons.arrow_upward_rounded,
-                              color: Colors.grey,
-                            ),
-                            Text("${pokemon.height}m"),
-                          ],
-                        ),
-                      ),
-                      Text(
-                        "Height",
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ),
-                // Moves
-                Container(
-                  margin: const EdgeInsets.only(left: 10, right: 10),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 70,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children:
-                              pokemon.abilities.map((e) => Text(e)).toList(),
-                        ),
-                      ),
-                      Text(
-                        "Moves",
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-          // Status
-          Container(
-            child: Column(
+        appBar: AppBar(
+            backgroundColor: mainColor,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "Base Status",
-                  style: TextStyle(
-                      color: mainColor,
-                      fontSize: 25,
-                      fontWeight: FontWeight.w600),
+                  title,
+                  style: const TextStyle(
+                      color: Colors.white, fontFamily: 'Pokemon'),
                 ),
-                Column(
-                  children: pokemon.stats.keys
-                      .map((key) => Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  width: 160,
-                                  child: Text(
-                                    "${key[0].toUpperCase()}${key.substring(1, key.length)}",
-                                    style: TextStyle(
-                                        color: mainColor,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ),
-                                Text(pokemon.stats[key].toString())
-                              ]))
-                      .toList(),
-                )
+                Text(
+                  pokemonNumber,
+                  style: const TextStyle(
+                      color: Colors.white, fontFamily: 'Pokemon'),
+                ),
               ],
-            ),
-          )
-        ],
-      ),
-    );
+            )),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Imagem
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                            bottomRight: Radius.circular(30)),
+                        color: mainColor,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [Image.network(pokemon.imageUrl)],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // Tipos
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: pokemon.types
+                      .map(
+                        (type) => Container(
+                          margin: const EdgeInsets.only(right: 3.0, left: 3.0),
+                          padding: const EdgeInsets.only(
+                              top: 2.0, right: 10.0, bottom: 2.0, left: 10.0),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(
+                              255,
+                              TypeToColor.convert(type)[0],
+                              TypeToColor.convert(type)[1],
+                              TypeToColor.convert(type)[2],
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(15.0)),
+                          ),
+                          child: Text(
+                            type[0].toUpperCase() +
+                                type.substring(1, type.length),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              // Peso, altura e habilidades
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Weight
+                    Container(
+                      margin: const EdgeInsets.only(left: 10, right: 10),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 70,
+                            child: Row(
+                              children: [
+                                const Icon(
+                                  Icons.balance_rounded,
+                                  color: Colors.grey,
+                                ),
+                                Text("${pokemon.weight}kg"),
+                              ],
+                            ),
+                          ),
+                          Text(
+                            "Weight",
+                            style: TextStyle(color: Colors.grey[600]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Height
+                    Container(
+                      margin: const EdgeInsets.only(left: 10, right: 10),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 70,
+                            child: Row(
+                              children: [
+                                const Icon(
+                                  Icons.arrow_upward_rounded,
+                                  color: Colors.grey,
+                                ),
+                                Text("${pokemon.height}m"),
+                              ],
+                            ),
+                          ),
+                          Text(
+                            "Height",
+                            style: TextStyle(color: Colors.grey[600]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Moves
+                    Container(
+                      margin: const EdgeInsets.only(left: 10, right: 10),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 70,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: pokemon.abilities
+                                  .map((e) => Text(e))
+                                  .toList(),
+                            ),
+                          ),
+                          Text(
+                            "Moves",
+                            style: TextStyle(color: Colors.grey[600]),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              // Status
+              Container(
+                margin: const EdgeInsets.only(bottom: 60),
+                child: Column(
+                  children: [
+                    Text(
+                      "Base Status",
+                      style: TextStyle(
+                          color: mainColor,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Column(
+                      children: pokemon.stats.keys
+                          .map((key) => Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      width: 160,
+                                      child: Text(
+                                        "${key[0].toUpperCase()}${key.substring(1, key.length)}",
+                                        style: TextStyle(
+                                            color: mainColor,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ),
+                                    Text(pokemon.stats[key].toString())
+                                  ]))
+                          .toList(),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
